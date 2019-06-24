c345844a1df69086ff89737be833ce160ec01be1
Cass07 <kjh95828@naver.com>
2019-06-25 00:44:01 +0900

    Add files via upload

--- indexSc.js
+++ indexSc.js
@@ -1,6 +1,9 @@
 document.getElementById('NpRecharge').addEventListener('click',function(){
     window.location = './npcharge/';
 })
+document.getElementById('NpTable').addEventListener('click',function(){
+    window.location = './nptable/';
+})
 document.getElementById('FeedbackLink').addEventListener('click',function(){
     window.open('https://gallog.dcinside.com/bakerst221/guestbook','_blank');
 })
