dc84b27c5192b310d5d782bd833bedd55c8d626c
Cass07 <kjh95828@naver.com>
2019-06-30 07:26:42 +0900

    Add files via upload

--- indexSc.js
+++ indexSc.js
@@ -4,6 +4,9 @@ document.getElementById('NpRecharge').addEventListener('click',function(){
 document.getElementById('NpTable').addEventListener('click',function(){
     window.location = './nptable/';
 })
+document.getElementById('CmdNpCharge').addEventListener('click',function(){
+    window.location = './cmdnpcharge/';
+})
 document.getElementById('FeedbackLink').addEventListener('click',function(){
     window.open('https://gallog.dcinside.com/bakerst221/guestbook','_blank');
 })
