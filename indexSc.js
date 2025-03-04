56f7e2bfeb7d88ab0ccd79441daa987c068e63b3
Cass <kjh95828@naver.com>
2025-03-04 15:36:04 +0900

    Update indexSc.js

--- indexSc.js
+++ indexSc.js
@@ -4,15 +4,9 @@ document.getElementById('NpRecharge').addEventListener('click',function(){
 document.getElementById('NpTable').addEventListener('click',function(){
     window.location = './nptable/';
 })
-document.getElementById('NpTableKor').addEventListener('click',function(){
-    window.location = './nptablekor/';
-})
 document.getElementById('CmdNpCharge').addEventListener('click',function(){
     window.location = './cmdnpcharge/';
 })
-document.getElementById('CmdStar').addEventListener('click',function(){
-    window.location = './test/';
-})
 document.getElementById('FeedbackLink').addEventListener('click',function(){
-    window.open('https://gall.dcinside.com/mgallery/board/view?id=takashitakeuchi&no=327142','_blank');
+    window.open('https://gall.dcinside.com/mgallery/board/view/?id=takashitakeuchi&no=2468817','_blank');
 })
